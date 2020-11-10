9d370d93ac62eb56255dd4dfee72aa77d8742748
abdull-rahman <abed472011@gmail.com>
2020-11-10 23:41:18 +0200

    added node sass

---
+++ src/components/admin/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/styles/Admin.scss';
+
+export default function AdminLogin(){
+    return (
+        <div className="container justify-content-center">
+            <div className="login-container">
+                <p>hello</p>
+            </div>
+        </div>
+    );
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
-import Home from '../components/Home.js'
-import admin from '../components/admin/index.js'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from "../components/Home";
+import admin from "../components/admin/index";
+import AdminLogin from "../components/admin/login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function Routes (){
-    return (
+function Routes() {
+  return (
     <Router>
       <Route path="/home" component={Home} />
       <Route path="/admin" component={admin} />
+      <Route path="/admin-login" component={AdminLogin} />
     </Router>
-    );
+  );
 }
 
 export default Routes;
